a7f8b82056e85f7f0f0564d5b2a4e5427c65f521
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-03-04 11:25:51 -0800

    quit work now

--- frontend/src/componets/Chat.tsx
+++ frontend/src/componets/Chat.tsx
@@ -8,21 +8,26 @@ import { Dictating } from "./Whisper";
 
 
 
-type WaitressProps = {
-    state: React.ReactNode;
-    dispatch: React.Dispatch<React.SetStateAction<JSX.Element | null>>;
-};
+// type WaitressProps = {
+//     state: React.ReactNode;
+//     dispatch: React.Dispatch<React.SetStateAction<JSX.Element | null>>;
+// };
 
-function Waitress({state, dispatch}: WaitressProps ) {
-    useEffect(() => {
-        const fetchConversation = async () => {
-            const conversation = await Conversation();
-            dispatch(conversation);
-        };
-        fetchConversation();
-    }, [dispatch]);
-    return <>{state}</>
-}
+// type CustomerProps = {
+//     state: string;
+//     dispatch: React.Dispatch<React.SetStateAction<string | null>>;
+// }
+
+// function Waitress({state, dispatch}: WaitressProps ) {
+//     useEffect(() => {
+//         const fetchConversation = async () => {
+//             const conversation = await Conversation();
+//             dispatch(conversation);
+//         };
+//         fetchConversation();
+//     }, [dispatch]);
+//     return <>{state}</>
+// }
 
 export function Chat() {
     const [response, setResponse] = useState<JSX.Element | null>(null);
@@ -32,7 +37,7 @@ export function Chat() {
         const fetchConversation = async () => {
             const conversation = await Conversation();
             setResponse(conversation);
-            setResponses([conversation]);
+            setResponses(responses => [...responses, conversation]);
         };
         if (!response) {
             fetchConversation();
@@ -40,11 +45,27 @@ export function Chat() {
     }, [response]);
 
 
-    const handleClick = async () => {
-        const conversation = await Conversation();
-        setResponse(conversation);
-        setResponses(prevResponses => [...prevResponses, conversation]);
+    const handleClick = () => {
+        setResponse(response);
     };
+
+
+    // const RespondAction = () => {
+    //     console.log(url);
+    //     // Using useEffect for single rendering
+    //     useEffect(() => {
+    //         // Using fetch to fetch the api from 
+    //         // flask server it will be redirected to proxy
+    //         fetch("/distate").then((res) =>
+    //             res.json().then((data) => {
+    //                 // Setting a data from api
+    //                 setTranscription(data);
+    //                 console.log(data);
+    //                 console.log(transcription);
+    //             })
+    //         );
+    //     }, []);
+    // }
 
     // Dictating part
     // const [isRecording, setIsRecording] = useState(false);
@@ -114,75 +135,116 @@ export function Chat() {
 
 
 
-    const [ url, setURL ] = useState("");
-    const [ transcription, setTranscription ] = useState<string>('');
-    const [ isRecording, setIsRecording ] = useState(false);
-    const [ audioBlob, setAudioBlob ] = useState<Blob | null>(null);
-    const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-    const chunks: Blob[] = [];
+    // const [ url, setURL ] = useState("");
+    // const [ transcription, setTranscription ] = useState<string>('');
+    // const [ isRecording, setIsRecording ] = useState(false);
+    // const [ audioBlob, setAudioBlob ] = useState<Blob | null>(null);
+    // const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+    // const chunks: Blob[] = [];
 
-    const handleRecord = () => {
-        if (!isRecording) {
-        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-            const mediaRecorder = new MediaRecorder(stream);
-            mediaRecorderRef.current = mediaRecorder;
-            mediaRecorder.start();
-            setIsRecording(true);
-        });
-        } else {
-            mediaRecorderRef.current?.stop();
-            setIsRecording(false);
-        }
-    };
+    // const handleRecord = () => {
+    //     if (!isRecording) {
+    //         navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+    //             const mediaRecorder = new MediaRecorder(stream);
+    //             mediaRecorderRef.current = mediaRecorder;
+    //             mediaRecorder.start();
+    //             setIsRecording(true);
+    //         });
+    //     } else {
+    //         mediaRecorderRef.current?.stop();
+    //         setIsRecording(false);
+    //     }
+    // };
 
-    const handleSave = () => {
-        if (audioBlob) {
-            setURL(URL.createObjectURL(audioBlob));
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = 'audio.wav';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-            URL.revokeObjectURL(url);
-        }
-    };
+    // const handleSave = () => {
+    //     if (audioBlob) {
+    //         setURL(URL.createObjectURL(audioBlob));
+    //         const link = document.createElement('a');
+    //         link.href = url;
+    //         link.download = 'audio.wav';
+    //         document.body.appendChild(link);
+    //         link.click();
+    //         document.body.removeChild(link);
+    //         URL.revokeObjectURL(url);
+    //     }
+    // };
 
-    const handleDataAvailable = (event: BlobEvent) => {
-        chunks.push(event.data);
-    };
+    // const handleDataAvailable = (event: BlobEvent) => {
+    //     chunks.push(event.data);
+    // };
+    // if (mediaRecorderRef.current) {
+    //     mediaRecorderRef.current.ondataavailable = handleDataAvailable;
+    //     mediaRecorderRef.current.onstop = () => {
+    //         const blob = new Blob(chunks, { type: 'audio/wav' });
+    //         setAudioBlob(blob);
+    //     };
+    // }
 
-    if (mediaRecorderRef.current) {
-        mediaRecorderRef.current.ondataavailable = handleDataAvailable;
-        mediaRecorderRef.current.onstop = () => {
-            const blob = new Blob(chunks, { type: 'audio/wav' });
-            setAudioBlob(blob);
-        };
-    }
+    // const dictating = async (event:any) => {
+    //     // const file = event.target.files[0];
+    //     // const data = new FormData();
+    //     // data.append('file', file);
+    //     const DEFAULT_PARAMS = {
+    //         "file": url,
+    //         "model": "whisper-1",
+    //     }
 
-    const dictating = async (event:any) => {
-        const file = event.target.files[0];
-        // const data = new FormData();
-        // data.append('file', file);
-        const DEFAULT_PARAMS = {
-            "file": file.name,
-            "model": "whisper-1",
-        }
+    //     const params_ = { ...DEFAULT_PARAMS};
+    //     const requestOptions = {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Authorization': 'Bearer ' + String(process.env.REACT_APP_API_KEY)
+    //         },
+    //         body: JSON.stringify(DEFAULT_PARAMS)
+    //     };
+    //     console.log(JSON.stringify(DEFAULT_PARAMS));
+    //     const response = await fetch('https://api.openai.com/v1/audio/transcriptions', requestOptions);
+    //     const responseText = await response.json();
+    //     console.log(responseText);
+    // }
 
-        const params_ = { ...DEFAULT_PARAMS};
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + String(process.env.REACT_APP_API_KEY)
-            },
-            body: JSON.stringify(params_)
-        };
-        console.log(params_);
-        const response = await fetch('https://api.openai.com/v1/audio/transcriptions', requestOptions);
-        const responseText = await response.json();
-        console.log(responseText);
-    }
+    // const { Configuration, OpenAIApi } = require("openai");
+
+    // const dictating = async (event:any) => {
+    //     const file = event.target.files[0];
+    //     const configuration = new Configuration({
+    //         // apiKey: process.env.REACT_APP_API_KEY,
+    //         Authorization: 'Bearer ' + String(process.env.REACT_APP_API_KEY)
+    //     });
+    //     const openai = new OpenAIApi(configuration);
+
+    //     const completion = await openai.createCompletion({
+    //         model: "whisper-1",
+    //         file: file,
+    //     });
+    //     console.log(completion.data.choices[0].text);
+    // }
+
+
+    // const dictating = async () => {
+    //     const formData = new FormData();
+    //     formData.append("file", url);
+    //     formData.append("model", "whisper-1");
+
+    //     const data = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+    //         headers:{
+    //             "Authorization": 'Bearer ' + String(process.env.REACT_APP_API_KEY),
+    //             "Content-Type": "multipart/form-data",
+    //         },
+    //         method: "POST",
+    //         body: formData,
+    //     })
+    //     .then((response) => response.json())
+    //     .then((result) => {
+    //         console.log(result);
+    //         setTranscription(result)
+    //         return result;
+    //     })
+    //     .catch((error) => {
+    //         console.error("Error:", error);
+    //     });
+    // }
     
 
     // const [recording, setRecording] = useState(false);
@@ -277,7 +339,7 @@ export function Chat() {
             <Button variant="primary" type="submit" onClick={handleClick}>
                 Start
             </Button>
-            <Waitress state={null} dispatch={() => {}} {...{ response, setResponse }} />
+            {/* <Waitress state={null} dispatch={() => {}} {...{ response, setResponse }} /> */}
             <div>
                 {responses.map((response, index) => (
                     <div key={index}>
@@ -285,13 +347,13 @@ export function Chat() {
                     </div>
                 ))}
             </div>
-            <button onClick={handleRecord}>{isRecording ? 'Stop' : 'Record'}</button>
+            {/* <button onClick={handleRecord}>{isRecording ? 'Stop' : 'Record'}</button> */}
             {/* <button onClick={onSpeechRecognition}>Start Speech Recognition</button> */}
 
-            <div>
-            <input type="file" accept="audio/*" onChange={dictating} />
+            {/* <div>
+                <input type="file" accept="audio/*" onChange={dictating} />
                 <p>{transcription}</p>
-            </div>
+            </div> */}
             {/* {recording ? (
                 <button onClick={handleStopRecording}>Stop recording</button>
             ) : (
@@ -311,18 +373,24 @@ export function Chat() {
             {audioBlob && (
                 <audio controls src={URL.createObjectURL(audioBlob)} />
             )}*/}
-            {/* <div> 
-            {isRecording ? (
+            {/* <div>  */}
+            {/* {isRecording ? (
                 <button onClick={handleRecord}>Stop recording</button>
             ) : (
                 <button onClick={handleRecord}>Start recording</button>
             )} */}
-            {/* {transcription && <p>{transcription}</p>}
-            <button onClick={handleRecord}>{isRecording ? 'Stop' : 'Record'}</button>
+            {/* <button onClick={handleRecord}>{isRecording ? 'Stop' : 'Record'}</button>
             <button onClick={handleSave} disabled={!audioBlob}>
                 Save Audio
-            </button> */}
-            {/* </div> */}
+            </button>
+            </div>
+            <Button variant="primary" type="submit" onClick={respondAction}>
+                Respond
+            </Button>
+            <div>
+                {transcription && <p>{transcription}</p>}
+            </div>
+             */}
         </Container>
     );
 }
--- frontend/src/componets/Whisper.tsx
+++ frontend/src/componets/Whisper.tsx
@@ -1,17 +1,22 @@
   
 export async function Dictating({params = {}, url}: any) {
-    const DEFAULT_PARAMS = {
-        "file": url,
-        "model": "whisper-1",
-    }
-    const params_ = { ...DEFAULT_PARAMS, ...params };
+    // const DEFAULT_PARAMS = {
+    //     "file": url,
+    //     "model": "whisper-1",
+    // }
+    const formData = new FormData();
+    formData.append("file", url);
+    formData.append("model", "whisper-1");
+    // console.log(url);
+    console.log(formData);
+    // const params_ = { ...DEFAULT_PARAMS, ...params };
     const requestOptions = {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'multipart/form-data',
             'Authorization': 'Bearer ' + String(process.env.REACT_APP_API_KEY)
         },
-        body: JSON.stringify(params_)
+        body: JSON.stringify(formData)
     };
     const response = await fetch('https://api.openai.com/v1/audio/transcriptions', requestOptions);
     const data = await response.json();
